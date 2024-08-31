c3c61658d27c743540d21857de6abdba315790ca
Fabio Pretel <fabio_pretel@outlook.com>
2024-08-30 22:48:54 -0300

    save

--- contador_lisboa/interacao/script.js
+++ contador_lisboa/interacao/script.js
@@ -1,17 +1,17 @@
 //Inicializa o contador com o valor armazenado no localStorage ou 0 se não houver valor armazenado
 let contador = parseInt(localStorage.getItem('contador')) || 0;
 
-        // Adiciona um ouvinte de evento de clique ao botão
-        const contadorElemento = document.getElementById('contador')
-        const botao = document.getElementById('botao')
+// Adiciona um ouvinte de evento de clique ao botão
+const contadorElemento = document.getElementById('contador')
+const botao = document.getElementById('botao')
 
-        // Atualiza o elemento do contador com o valor inicial
-        contadorElemento.textContent = contador;
+// Atualiza o elemento do contador com o valor inicial
+contadorElemento.textContent = contador;
 
-        // Adiciona um ouvinte de evento de clique ao botão
-        botao.addEventListener('click', function() {
-            contador++; // Incrementa o contador
-            contadorElemento.textContent = contador; // Atualiza o elemento do contador com o novo valor
-            localStorage.setItem('contador', contador); // Salva o novo valor no localStorage
-        });
+// Adiciona um ouvinte de evento de clique ao botão
+botao.addEventListener('click', function() {
+    contador++; // Incrementa o contador
+    contadorElemento.textContent = contador; // Atualiza o elemento do contador com o novo valor
+    localStorage.setItem('contador', contador); // Salva o novo valor no localStorage
+});
 
