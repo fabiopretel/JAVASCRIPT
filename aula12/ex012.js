4e316ef64638759b4a97c7b7dc5c227af9f3f532
Fabio Pretel <fabio_pretel@outlook.com>
2024-10-07 23:46:37 -0300

    save all

---
+++ aula12/ex012.js
@@ -0,0 +1,19 @@
+var agora = new Date()
+var hora = agora.getHours()
+var minuto = agora.getMinutes()
+var segundo = agora.getSeconds()
+
+console.log(`Agora são exatamente ${hora}:${minuto} horas.`)
+
+if(hora >= 6 && hora < 12){
+    console.log('Bom Dia!')
+}
+else if(hora <= 18){
+    console.log('Boa Tarde!')
+}
+else if(hora < 24){
+    console.log('Boa Noite!')
+}
+else{
+    console.log('Boa Madrugada!')
+}
