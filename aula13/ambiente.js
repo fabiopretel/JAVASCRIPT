4c368a60a04e4a7ff2a8600893ee2d7d1e395ac7
Fabio Pretel <fabio_pretel@outlook.com>
2024-10-09 08:24:36 -0300

    aula13

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+var a = 1
+while(a <= 6){
+    console.log(`Passo ${a}`)
+    a++
+}
+do{
+    console.log(`Passo ${a}`)
+    a++
+} while(a <= 6)
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
