7717daae5295a5a2159fbb6cab27456d845bef44
Fabio Pretel <fabio_pretel@outlook.com>
2024-10-09 08:54:44 -0300

    aula14

---
+++ aula14/ambiente.js
@@ -0,0 +1,13 @@
+// inicio ; teste; incremento
+/*
+for(var a=1; a<=5; a++){
+    console.log(a)
+}
+*/
+
+//modo depuração
+console.log('Vai começar...')
+for(var a=1; a<=5; a++){
+    console.log(a)
+}
+console.log('Fim!')
