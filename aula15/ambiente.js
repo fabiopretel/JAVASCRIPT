be6b626ff29d7c3d5aaa18125d45ccd8ff251976
Fabio Pretel <fabio_pretel@outlook.com>
2024-12-11 20:22:37 -0300

    save

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -14,7 +14,7 @@ else{
 }
 
 
-/*
+
 let valores = [8, 1, 7, 4, 2, 9]
 console.log(valores)
 for(let pos=0; pos<valores.length; pos++){
@@ -24,4 +24,3 @@ for(let pos=0; pos<valores.length; pos++){
 for(let pos in valores){
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
-*/
